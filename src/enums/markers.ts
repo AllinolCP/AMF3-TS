3ddffb1f1bcd783731f477293800a0d105101140
Daan <24899039+Zaseth@users.noreply.github.com>
2020-10-10 14:05:01 +0200

    Finished markers

---
+++ src/enums/markers.ts
@@ -0,0 +1,24 @@
+/**
+ * @exports
+ * @enum
+ */
+export enum Markers {
+  UNDEFINED = 0x00,
+  NULL = 0x01,
+  FALSE = 0x02,
+  TRUE = 0x03,
+  INTEGER = 0x04,
+  DOUBLE = 0x05,
+  STRING = 0x06,
+  XML_DOCUMENT = 0x07,
+  DATE = 0x08,
+  ARRAY = 0x09,
+  OBJECT = 0x0A,
+  XML = 0x0B,
+  BYTEARRAY = 0x0C,
+  VECTOR_INT = 0x0D,
+  VECTOR_UINT = 0x0E,
+  VECTOR_DOUBLE = 0x0F,
+  VECTOR_OBJECT = 0x10,
+  DICTIONARY = 0x011
+}

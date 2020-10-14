d003742dec73f5bdb4fbba358c849eb41bda0fb4
Daan <24899039+Zaseth@users.noreply.github.com>
2020-10-14 18:51:57 +0200

    Add stream

---
+++ src/core/stream.ts
@@ -0,0 +1,502 @@
+import { Sizes } from '../enums/sizes';
+
+/**
+ * @exports
+ * @class
+ */
+export class Stream {
+  /**
+   * @public
+   * @description The array holding the data
+   * @type {Array<number>}
+   */
+  public data: number[];
+  /**
+   * @public
+   * @description The position in the array
+   * @type {number}
+   */
+  public position: number;
+
+  /**
+   * @constructor
+   * @param {Array<number>} data
+   */
+  constructor(data: number[] = []) {
+    /**
+     * @description Initialize the data holder
+     * @type {Array<number>}
+     */
+    this.data = data;
+    /**
+     * @description Initialize the position
+     * @type {number}
+     */
+    this.position = 0;
+  }
+
+  /**
+   * @description Returns the length of the data holder
+   * @returns {number}
+   */
+  get length(): number {
+    return this.data.length;
+  }
+
+  /**
+   * @description Returns the amount of bytes available
+   * @returns {number}
+   */
+  get bytesAvailable(): number {
+    return this.length - this.position;
+  }
+
+  /**
+   * @private
+   * @description Checks if the given value can be written
+   * @param {number} value
+   * @param {number} min
+   * @param {number} max
+   * @returns {void}
+   */
+  private canWrite(value: number, min: number, max: number): void {
+    if (value < min || value > max) {
+      throw new RangeError(`The value: '${value}' is out of range (${min}, ${max}).`);
+    }
+  }
+
+  /**
+   * @private
+   * @description Checks if the given integer size can be read
+   * @param {number} size
+   * @returns {void}
+   */
+  private canRead(size: number): void {
+    const available: number = this.bytesAvailable;
+
+    if (available === 0) {
+      throw new RangeError(`There's no data left to read.`);
+    }
+
+    if (available < size) {
+      throw new RangeError(`Couldn't read an integer of '${size * 8}' bits as there's only '${available}' bytes available.`);
+    }
+  }
+
+  /**
+   * @public
+   * @static
+   * @description Returns the byte length of a string
+   * @param {string} value
+   * @returns {number}
+   */
+  public static byteLength(value: string): number {
+    return new TextEncoder().encode(value).length;
+  }
+
+  /**
+   * @public
+   * @description Writes a signed 8-bit integer
+   * @param {number} value
+   * @returns {void}
+   */
+  public writeByte(value: number): void {
+    this.canWrite(value, Sizes.INT8_MIN, Sizes.INT8_MAX);
+
+    this.data[this.position++] = value;
+  }
+
+  /**
+   * @public
+   * @description Reads a signed 8-bit integer
+   * @returns {number}
+   */
+  public readByte(): number {
+    this.canRead(1);
+
+    const value: number = this.data[this.position++];
+
+    this.canWrite(value, Sizes.INT8_MIN, Sizes.INT8_MAX);
+
+    return value;
+  }
+
+  /**
+   * @public
+   * @description Writes an unsigned 8-bit integer
+   * @param {number} value
+   * @returns {void}
+   */
+  public writeUnsignedByte(value: number): void {
+    this.canWrite(value, Sizes.UINT8_MIN, Sizes.UINT8_MAX);
+
+    this.data[this.position++] = value;
+  }
+
+  /**
+   * @public
+   * @description Reads an unsigned 8-bit integer
+   * @returns {number}
+   */
+  public readUnsignedByte(): number {
+    this.canRead(1);
+
+    const value: number = this.data[this.position++];
+
+    this.canWrite(value, Sizes.UINT8_MIN, Sizes.UINT8_MAX);
+
+    return value;
+  }
+
+  /**
+   * @public
+   * @description Writes a signed 16-bit integer
+   * @param {number} value
+   * @returns {void}
+   */
+  public writeShort(value: number): void {
+    this.canWrite(value, Sizes.INT16_MIN, Sizes.INT16_MAX);
+
+    this.data[this.position++] = (value >>> 8);
+    this.data[this.position++] = value;
+  }
+
+  /**
+   * @public
+   * @description Reads a signed 16-bit integer
+   * @returns {number}
+   */
+  public readShort(): number {
+    this.canRead(2);
+
+    const value: number = (this.data[this.position++] << 8) | (this.data[this.position++]);
+
+    this.canWrite(value, Sizes.INT16_MIN, Sizes.INT16_MAX);
+
+    return value;
+  }
+
+  /**
+   * @public
+   * @description Writes an unsigned 16-bit integer
+   * @param {number} value
+   * @returns {void}
+   */
+  public writeUnsignedShort(value: number): void {
+    this.canWrite(value, Sizes.UINT16_MIN, Sizes.UINT16_MAX);
+
+    this.data[this.position++] = (value >>> 8);
+    this.data[this.position++] = value;
+  }
+
+  /**
+   * @public
+   * @description Reads an unsigned 16-bit integer
+   * @returns {number}
+   */
+  public readUnsignedShort(): number {
+    this.canRead(2);
+
+    const value: number = (this.data[this.position++] << 8) | (this.data[this.position++]);
+
+    this.canWrite(value, Sizes.UINT16_MIN, Sizes.UINT16_MAX);
+
+    return value;
+  }
+
+  /**
+   * @public
+   * @description Writes a signed 32-bit integer
+   * @param {number} value
+   * @returns {void}
+   */
+  public writeInt(value: number): void {
+    this.canWrite(value, Sizes.INT32_MIN, Sizes.INT32_MAX);
+
+    this.data[this.position++] = (value >>> 24);
+    this.data[this.position++] = (value >>> 16);
+    this.data[this.position++] = (value >>> 8);
+    this.data[this.position++] = value;
+  }
+
+  /**
+   * @public
+   * @description Reads a signed 32-bit integer
+   * @returns {number}
+   */
+  public readInt(): number {
+    this.canRead(4);
+
+    const value: number = (this.data[this.position++] << 24)
+      | (this.data[this.position++] << 16)
+      | (this.data[this.position++] << 8)
+      | (this.data[this.position++]);
+
+    this.canWrite(value, Sizes.INT32_MIN, Sizes.INT32_MAX);
+
+    return value;
+  }
+
+  /**
+   * @public
+   * @description Writes an unsigned 32-bit integer
+   * @param {number} value
+   * @returns {void}
+   */
+  public writeUnsignedInt(value: number): void {
+    this.canWrite(value, Sizes.UINT32_MIN, Sizes.UINT32_MAX);
+
+    this.data[this.position++] = (value >>> 24);
+    this.data[this.position++] = (value >>> 16);
+    this.data[this.position++] = (value >>> 8);
+    this.data[this.position++] = value;
+  }
+
+  /**
+   * @public
+   * @description Reads an unsigned 32-bit integer
+   * @returns {number}
+   */
+  public readUnsignedInt(): number {
+    this.canRead(4);
+
+    const value: number = (this.data[this.position++] << 24)
+      | (this.data[this.position++] << 16)
+      | (this.data[this.position++] << 8)
+      | (this.data[this.position++]);
+
+    this.canWrite(value, Sizes.UINT32_MIN, Sizes.UINT32_MAX);
+
+    return value;
+  }
+
+  /**
+   * @public
+   * @description Writes an UTF8 string
+   * @param {string} value
+   * @returns {void}
+   */
+  public writeUTF(value: string): void {
+    const bytes: number[] = Array.from(new TextEncoder().encode(value));
+
+    this.writeUnsignedShort(bytes.length);
+
+    this.position += bytes.length;
+    this.data = this.data.concat(bytes);
+  }
+
+  /**
+   * @public
+   * @description Reads an UTF8 string
+   * @returns {string}
+   */
+  public readUTF(): string {
+    this.canRead(2);
+
+    const length: number = this.readUnsignedShort();
+    const bytes: Uint8Array = Uint8Array.from(this.data.slice(this.position, this.position + length));
+
+    this.position += bytes.length;
+
+    return new TextDecoder().decode(bytes);
+  }
+
+  /**
+   * @public
+   * @description Writes an UTF8 string without the length
+   * @param {string} value
+   * @returns {void}
+   */
+  public writeUTFBytes(value: string): void {
+    const bytes: number[] = Array.from(new TextEncoder().encode(value));
+
+    this.position += bytes.length;
+    this.data = this.data.concat(bytes);
+  }
+
+  /**
+   * @public
+   * @description Reads an amount of UTF8 bytes based on the length
+   * @param {number} length
+   * @returns {string}
+   */
+  public readUTFBytes(length: number): string {
+    this.canRead(length);
+
+    const bytes: Uint8Array = Uint8Array.from(this.data.slice(this.position, this.position + length));
+
+    this.position += length;
+
+    return new TextDecoder().decode(bytes);
+  }
+
+  /**
+   * @public
+   * @description Writes a signed 32-bit float
+   * @param {number} value
+   * @returns {void}
+   */
+  public writeFloat(value: number): void {
+    const dv: DataView = new DataView(new ArrayBuffer(4));
+    dv.setFloat32(0, value);
+
+    const bytes: number[] = [...new Uint8Array(dv.buffer)];
+
+    this.position += 4;
+    this.data = this.data.concat(bytes);
+  }
+
+  /**
+   * @public
+   * @description Reads a signed 32-bit float
+   * @returns {number}
+   */
+  public readFloat(): number {
+    this.canRead(4);
+
+    const bytes: number[] = this.data.slice(this.position, this.position + 4);
+    const value: number = new Float32Array(Uint8Array.from(bytes.reverse()).buffer)[0];
+
+    this.position += 4;
+
+    return value;
+  }
+
+  /**
+   * @public
+   * @description Writes a signed 64-bit float
+   * @param {number} value
+   * @returns {void}
+   */
+  public writeDouble(value: number): void {
+    const dv: DataView = new DataView(new ArrayBuffer(8));
+    dv.setFloat64(0, value);
+
+    const bytes: number[] = [...new Uint8Array(dv.buffer)];
+
+    this.position += 8;
+    this.data = this.data.concat(bytes);
+  }
+
+  /**
+   * @public
+   * @description Reads a signed 64-bit float
+   * @returns {number}
+   */
+  public readDouble(): number {
+    this.canRead(8);
+
+    const bytes: number[] = this.data.slice(this.position, this.position + 8);
+    const value: number = new Float64Array(Uint8Array.from(bytes.reverse()).buffer)[0];
+
+    this.position += 8;
+
+    return value;
+  }
+
+  /**
+   * @public
+   * @description Writes a boolean
+   * @param {boolean} value
+   * @returns {void}
+   */
+  public writeBoolean(value: boolean): void {
+    this.writeByte(value ? 1 : 0);
+  }
+
+  /**
+   * @public
+   * @description Reads a boolean
+   * @returns {boolean}
+   */
+  public readBoolean(): boolean {
+    return this.readByte() !== 0;
+  }
+
+  /**
+   * @public
+   * @description Writes multiple signed bytes
+   * @param {Stream} stream
+   * @param {number} offset
+   * @param {number} length
+   * @returns {void}
+   */
+  public writeBytes(stream: Stream, offset: number = 0, length: number = 0): void {
+    if (length === 0) {
+      length = stream.length - offset;
+    }
+
+    for (let i: number = 0; i < length; i++) {
+      this.data[i + this.position] = stream.data[i + offset];
+    }
+
+    this.position += length;
+  }
+
+  /**
+   * @public
+   * @description Reads multiple signed bytes
+   * @param {Stream} stream
+   * @param {number} offset
+   * @param {number} length
+   * @returns {void}
+   */
+  public readBytes(stream: Stream, offset: number = 0, length: number = 0): void {
+    if (length === 0) {
+      length = this.bytesAvailable;
+    }
+
+    for (let i: number = 0; i < length; i++) {
+      stream.data[i + offset] = this.data[i + this.position];
+    }
+
+    this.position += length;
+  }
+
+  /**
+   * @public
+   * @description Writes an unsigned 29-bit integer
+   * @param {number} value
+   * @returns {void}
+   */
+  public writeUInt29(value: number): void {
+    if (value < Sizes.UINT29_1) {
+      this.writeUnsignedByte(value);
+    } else if (value < Sizes.UINT29_2) {
+      this.writeUnsignedByte(((value >> 7) & Sizes.INT8_MAX) | Sizes.UINT29_1);
+      this.writeUnsignedByte(value & Sizes.INT8_MAX);
+    } else if (value < Sizes.UINT29_3) {
+      this.writeUnsignedByte(((value >> 14) & Sizes.INT8_MAX) | Sizes.UINT29_1);
+      this.writeUnsignedByte(((value >> 7) & Sizes.INT8_MAX) | Sizes.UINT29_1);
+      this.writeUnsignedByte(value & Sizes.INT8_MAX);
+    } else if (value < Sizes.UINT29_4) {
+      this.writeUnsignedByte(((value >> 22) & Sizes.INT8_MAX) | Sizes.UINT29_1);
+      this.writeUnsignedByte(((value >> 15) & Sizes.INT8_MAX) | Sizes.UINT29_1);
+      this.writeUnsignedByte(((value >> 8) & Sizes.INT8_MAX) | Sizes.UINT29_1);
+      this.writeUnsignedByte(value & Sizes.UINT8_MAX);
+    } else {
+      throw new RangeError(`The value: '${value}' is out of range.`);
+    }
+  }
+
+  /**
+   * @public
+   * @description Reads an unsigned 29-bit integer
+   * @returns {number}
+   */
+  public readUInt29(): number {
+    let value: number = 0;
+
+    for (let i: number = 0; i < 4; i++) {
+      const byte: number = this.readUnsignedByte();
+
+      value = i === 3 ? (value << 8) + byte : (value << 7) + (byte & Sizes.INT8_MAX);
+
+      if (!(byte & Sizes.UINT29_1)) {
+        break;
+      }
+    }
+
+    return value;
+  }
+}
---
+++ src/enums/sizes.ts
@@ -0,0 +1,28 @@
+/**
+ * @exports
+ * @enum
+ */
+export enum Sizes {
+  INT8_MIN = -128,
+  INT8_MAX = 127,
+
+  UINT8_MIN = 0,
+  UINT8_MAX = 255,
+
+  INT16_MIN = -32768,
+  INT16_MAX = 32767,
+
+  UINT16_MIN = 0,
+  UINT16_MAX = 65535,
+
+  UINT29_1 = 128,
+  UINT29_2 = 16384,
+  UINT29_3 = 2097152,
+  UINT29_4 = 1073741824,
+
+  INT32_MIN = -2147483648,
+  INT32_MAX = 2147483647,
+
+  UINT32_MIN = 0,
+  UINT32_MAX = 4294967295
+}

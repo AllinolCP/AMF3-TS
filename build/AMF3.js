17c8fda98d711126db0cd94e325fbae4bb137eb6
Daan <24899039+Zaseth@users.noreply.github.com>
2020-10-22 13:50:09 +0200

    Build Stream global

--- build/AMF3.js
+++ build/AMF3.js
@@ -1,6 +1,7 @@
 'use strict';
 
 window.AMF3 = require('../dist/AMF3').AMF3;
+window.Stream = require('../dist/core/stream').Stream;
 
 if (!window.TextEncoder) {
   require('./EncoderDecoderTogether.min');
